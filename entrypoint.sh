09c194191b1bfccb7cdc8b7884a06539695a93cc
fabbo <fscherle98@gmail.com>
2025-02-18 10:28:08 +0100

    Enable HTTP for Keycloak startup in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start Keycloak in the background
-/opt/bitnami/keycloak/bin/kc.sh start &
+/opt/bitnami/keycloak/bin/kc.sh start --http-enabled=true &
 
 # Wait for Keycloak to be ready
 until curl -s http://localhost:8080/realms/master; do
