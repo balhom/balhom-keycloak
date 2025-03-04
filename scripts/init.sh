5068770e59425b34baf95d6b73e7e4875e428c97
fabbo-repo <fscherle98@gmail.com>
2025-03-04 17:59:18 +0100

    Enhance Keycloak initialization script to conditionally create user and clients based on environment variables

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,11 +8,12 @@ until curl -s -o /dev/null http://localhost:8080; do
     sleep 5
 done
 
+# Default values for Keycloak initialization
+: "${KEYCLOAK_INIT_REALM:=balhom-realm}"
+: "${KEYCLOAK_INIT_CLIENT:=balhom-client}"
+: "${KEYCLOAK_INIT_API_CLIENT:=balhom-api-client}"
+
 export PATH=$PATH:/opt/bitnami/keycloak/bin
-
-# More info on how to use kcadm.sh on:
-# https://access.redhat.com/documentation/en-us/red_hat_single_sign-on/7.5/html/server_administration_guide/admin_cli
-
 
 echo "Starting initialization of Keycloak server"
 
@@ -25,33 +26,33 @@ kcadm.sh config credentials --server http://localhost:8080 --realm master \
 ## Realm creation step
 if ! kcadm.sh get realms/${KEYCLOAK_INIT_REALM} &>/dev/null; then
   echo "Creating '${KEYCLOAK_INIT_REALM}' realm"
-  
-  kcadm.sh create realms -s realm="${KEYCLOAK_INIT_REALM}" -s enabled=true -s registrationEmailAsUsername=true \
-    -s loginWithEmailAllowed=true -s smtpServer.host=${KEYCLOAK_INIT_MAILHOG_HOST} -s smtpServer.port=1025 \
-    -s smtpServer.auth=false -s smtpServer.ssl=none -s smtpServer.replyTo=${KEYCLOAK_INIT_SMTP_RECEIVER} \
-    -s smtpServer.from=${KEYCLOAK_INIT_SMTP_SENDER}
 
+  # Set BalHom login theme
   kcadm.sh update realms/${KEYCLOAK_INIT_REALM} -s loginTheme=balhom-keycloak-theme
 
   # Init realm user
-  echo "Creating '${KEYCLOAK_INIT_USER}' user in '${KEYCLOAK_INIT_REALM}' realm"
-  
-  kcadm.sh create users -r "${KEYCLOAK_INIT_REALM}" -s username="${KEYCLOAK_INIT_USER}" -s enabled=true \
-    -s email="${KEYCLOAK_INIT_USER}" -s emailVerified=true -s firstName="Test" -s lastName="Test"
-  
-  kcadm.sh set-password -r "${KEYCLOAK_INIT_REALM}" --username "${KEYCLOAK_INIT_USER}" \
-    --new-password "${KEYCLOAK_INIT_USER_PASSWORD}"
-  
-  echo "Created '${KEYCLOAK_INIT_USER}' user in '${KEYCLOAK_INIT_REALM}' realm"
+  if [[ -n "${KEYCLOAK_INIT_USER}" && -n "${KEYCLOAK_INIT_USER_PASSWORD}" ]]; then
+    echo "Creating '${KEYCLOAK_INIT_USER}' user in '${KEYCLOAK_INIT_REALM}' realm"
+    
+    kcadm.sh create users -r "${KEYCLOAK_INIT_REALM}" -s username="${KEYCLOAK_INIT_USER}" -s enabled=true \
+      -s email="${KEYCLOAK_INIT_USER}" -s emailVerified=true -s firstName="Test" -s lastName="Test"
+    
+    kcadm.sh set-password -r "${KEYCLOAK_INIT_REALM}" --username "${KEYCLOAK_INIT_USER}" \
+      --new-password "${KEYCLOAK_INIT_USER_PASSWORD}"
+    
+    echo "Created '${KEYCLOAK_INIT_USER}' user in '${KEYCLOAK_INIT_REALM}' realm"
 
-  # Create client for WEB
-  echo "Creating '${KEYCLOAK_INIT_CLIENT}' client"
+    # Create client for WEB
+    echo "Creating '${KEYCLOAK_INIT_CLIENT}' client"
 
-  kcadm.sh create clients -r "${KEYCLOAK_INIT_REALM}" -s clientId="${KEYCLOAK_INIT_CLIENT}" -s enabled=true -s \
-    publicClient=true -s redirectUris='["*"]' -s webOrigins='["*"]' -s directAccessGrantsEnabled=true \
-    -s standardFlowEnabled=true
-  
-  echo "Created new client '${KEYCLOAK_INIT_CLIENT}'"
+    kcadm.sh create clients -r "${KEYCLOAK_INIT_REALM}" -s clientId="${KEYCLOAK_INIT_CLIENT}" -s enabled=true -s \
+      publicClient=true -s redirectUris='["*"]' -s webOrigins='["*"]' -s directAccessGrantsEnabled=true \
+      -s standardFlowEnabled=true
+    
+    echo "Created new client '${KEYCLOAK_INIT_CLIENT}'"
+  else
+    echo "Skipping user creation as KEYCLOAK_INIT_USER or KEYCLOAK_INIT_USER_PASSWORD is not set"
+  fi
   
   # Create client for API
   echo "Creating '${KEYCLOAK_INIT_API_CLIENT}' client"
